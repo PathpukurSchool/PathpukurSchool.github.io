10618b9adfca4329a1516dea21287b9124e8ce3b
PathpukurSchool <1954pathpukur@gmail.com>
2025-05-25 06:25:06 +0530

    Add files via upload

---
+++ dates.js
@@ -0,0 +1,7 @@
+// dates.js
+const examDates = [
+  { text: "* 1st Exam 25-06-2025 *", color: "#e53935" },
+  { text: "* 2nd Exam 26-06-2025 *", color: "#fb8c00" },
+  { text: "* Test Exam (X) 27-06-2025 *", color: "#43a047" },
+  { text: "* 3rd Exam 28-06-2025 *", color: "#1e88e5" }
+];
