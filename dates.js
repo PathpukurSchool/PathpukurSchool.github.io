566113a5e104b7eda374ab06fd54cb0254ba56d8
PathpukurSchool <1954pathpukur@gmail.com>
2025-05-29 05:05:25 +0530

    Update dates.js

--- dates.js
+++ dates.js
@@ -1,7 +1,7 @@
-// dates.js
+// last date of marks submission (Teacher)
 const examDates = [
-  { text: "* 1st Exam __/__/2025 *", color: "#e53935", backgroundColor: "" },
-  { text: "* 2nd Exam __/__/2025 *", color: "#43a047", backgroundColor: "" },
-  { text: "* Test Exam (X) __/__/2025 *", color: "#fb8c00", backgroundColor: "" },
-  { text: "* 3rd Exam __/__/2025 *", color: "#1e88e5", backgroundColor: "" }
+  { text: "1st Exam", date: "27-06-2025", color: "blue", backgroundColor: "" },
+  { text: "2nd Exam", date: "", color: "green", backgroundColor: "" },
+  { text: "Test Exam (X)", date: "29-09-2025", color: "orange", backgroundColor: "" },
+  { text: "3rd Exam", date: "", color: "red", backgroundColor: "" }
 ];
